aaeb7faa787eada55b7eaff92c20697784229738
Pietro Bucker <pietro_177@hotmail.com>
2023-07-25 16:53:51 -0300

    removido runnertime

--- src/models/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-# teste = Dish('lasanha', 10)
-# teste.add_ingredient_dependency(Ingredient('farinha'), 10)
-# print(teste.recipe.get(Ingredient('farinha')))
-# print(teste.get_restrictions())
--- src/services/tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-
-teste = MenuBuilder(DATA_PATH, INVENTORY_PATH)
-print(teste.get_main_menu())
